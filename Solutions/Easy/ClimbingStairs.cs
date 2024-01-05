913a6cbf4b8c2c675f1ef63944023ea2ab0497c5
Victor Florescu <victor.florescu@amdaris.com>
2024-01-05 08:28:58 +0200

    Climbing Stairs

---
+++ Solutions/Easy/ClimbingStairs.cs
@@ -0,0 +1,26 @@
+﻿namespace Sandbox.Solutions.Easy;
+
+public class ClimbingStairs
+{
+    public int ClimbStairs(int n)
+    {
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+
+        int previousStep = 1;
+        int currentStep = 2;
+
+        int nextStep = 0;
+
+        for (int i = 2; i < n; i++)
+        {
+            nextStep = currentStep + previousStep;
+            previousStep = currentStep;
+            currentStep = nextStep;
+        }
+
+        return nextStep;
+    }
+}
