30d257feb0620ae0cbf5d6c6c2bbeae8d36a3713
Victor Florescu <victor.florescu@amdaris.com>
2024-02-03 08:34:08 +0200

    Maximum subarray - kadane algorithm

---
+++ Solutions/Medium/MaximumSubarray.cs
@@ -0,0 +1,19 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class MaximumSubarray
+{
+    public int MaxSubArray(int[] nums)
+    {
+        // kadane's algorithm
+        var localMax = nums[0];
+        var max = nums[0];
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            localMax = Math.Max(nums[i], nums[i] + localMax);
+            max = Math.Max(max, localMax);
+        }
+
+        return max;
+    }
+}
