9424d039810ad37438bc2f6e96134d4570bf8337
Victor Florescu <victor.florescu@amdaris.com>
2024-02-16 10:39:50 +0200

    Best Time to Buy and Sell Stock II

---
+++ Solutions/Medium/BestTimeToBuyAndSellStock2.cs
@@ -0,0 +1,18 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class BestTimeToBuyAndSellStock2
+{
+    public int MaxProfit(int[] prices)
+    {
+        // you can sell and buy immediately on the same day
+        // buy low - sell high
+        var maxProfit = 0;
+        for (var i = 1; i < prices.Length; i++)
+        {
+            if (prices[i] > prices[i - 1])
+                maxProfit += prices[i] - prices[i - 1];
+        }
+
+        return maxProfit;
+    }
+}
