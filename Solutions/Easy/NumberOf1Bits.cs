e27d4387d4373113bf09ebdd4436b369dd5d0b7d
Victor Florescu <victor.florescu@amdaris.com>
2024-03-31 13:57:31 +0300

    Number of 1 Bits

---
+++ Solutions/Easy/NumberOf1Bits.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+
+namespace Sandbox.Solutions.Easy;
+
+public class NumberOf1Bits
+{
+    public int HammingWeight(int n)
+    {
+        var bitArray = new BitArray(new[] { n });
+        return bitArray.Cast<bool>().Count(b => b);
+    }
+}
