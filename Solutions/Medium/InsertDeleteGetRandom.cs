c7a4ffbfbcb1f2e79bd88867e789844954a6c66c
Victor Florescu <victor.florescu@amdaris.com>
2024-05-10 13:52:11 +0300

    Insert Delete GetRandom O(1)

---
+++ Solutions/Medium/InsertDeleteGetRandom.cs
@@ -0,0 +1,27 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class RandomizedSet
+{
+    private readonly HashSet<int> _hashSet;
+
+    public RandomizedSet()
+    {
+        _hashSet = new HashSet<int>();
+    }
+
+    public bool Insert(int val)
+    {
+        return _hashSet.Add(val);
+    }
+
+    public bool Remove(int val)
+    {
+        return _hashSet.Remove(val);
+    }
+
+    public int GetRandom()
+    {
+        var rnd = new Random().Next(0, _hashSet.Count);
+        return _hashSet.ToArray()[rnd];
+    }
+}
