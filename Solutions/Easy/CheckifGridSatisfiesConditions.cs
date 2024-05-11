2e0087ce93f1618014b9cb70d7e3f5ea08b8ebcf
Victor Florescu <victor.florescu@amdaris.com>
2024-05-11 18:50:52 +0300

    Check if grid satisfied conditions

---
+++ Solutions/Easy/CheckifGridSatisfiesConditions.cs
@@ -0,0 +1,21 @@
+﻿namespace Sandbox.Solutions.Easy;
+
+public class CheckifGridSatisfiesConditions
+{
+    public bool SatisfiesConditions(int[][] grid)
+    {
+        for (int i = 0; i < grid.Length; i++)
+        {
+            for (int j = 0; j < grid[i].Length; j++)
+            {
+                if (i != grid.Length - 1 && grid[i][j] != grid[i + 1][j])
+                    return false;
+
+                if (j != grid[i].Length - 1 && grid[i][j] == grid[i][j + 1])
+                    return false;
+            }
+        }
+
+        return true;
+    }
+}
