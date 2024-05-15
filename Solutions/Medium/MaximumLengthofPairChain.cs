0dac50c9599cc04090ab5c3c325133c336ad1bb7
Victor Florescu <victor.florescu@amdaris.com>
2024-05-15 08:51:28 +0300

    Maximum Length of Pair Chain - dp and grredy two solutions

---
+++ Solutions/Medium/MaximumLengthofPairChain.cs
@@ -0,0 +1,48 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class MaximumLengthofPairChain
+{
+    public int FindLongestChain(int[][] pairs)
+    {
+        Array.Sort(pairs, (ints, ints1) => ints[1].CompareTo(ints1[1]));
+
+        var dp = new int[pairs.Length];
+        Array.Fill(dp, 1);
+
+        for (var i = 0; i < pairs.Length; i++)
+        {
+            var start = pairs[i][0];
+
+            for (var j = 0; j < i; j++)
+            {
+                var fromEnd = pairs[j][1];
+
+                if (fromEnd < start)
+                {
+                    dp[i] = Math.Max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return dp[^1];
+    }
+
+    public int FindLongestChainGreedy(int[][] pairs)
+    {
+        Array.Sort(pairs, (ints, ints1) => ints[1].CompareTo(ints1[1]));
+
+        // remove overlapping intervals
+        var result = 0;
+        var previous = int.MinValue;
+        foreach (var pair in pairs)
+        {
+            if (pair[0] > previous)
+            {
+                result++;
+                previous = pair[1];
+            }
+        }
+
+        return result;
+    }
+}
