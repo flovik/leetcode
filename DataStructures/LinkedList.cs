9b8ae47b407b985059029b3f2a728dee0f640283
Victor Florescu <victor.florescu@amdaris.com>
2024-06-14 16:04:04 +0300

    some linked list playing

---
+++ DataStructures/LinkedList.cs
@@ -0,0 +1,20 @@
+﻿namespace Sandbox.DataStructures;
+
+public class LinkedList
+{
+    private LinkedList<int> _linkedList = new();
+
+    public void WorkWithLinkedList()
+    {
+        var list = new List<int> { 1, 2, 3, 4, 5 };
+        _linkedList = new LinkedList<int>(list);
+
+        _linkedList.AddLast(6);
+        _linkedList.AddFirst(-1);
+
+        _linkedList.RemoveFirst();
+        _linkedList.RemoveLast();
+
+        var first = _linkedList.First;
+    }
+}
---
+++ Topics/LinkedList/LinkedList.cs
@@ -0,0 +1,58 @@
+﻿namespace Sandbox.Topics.LinkedList;
+
+// complexity
+// find - O(n)
+// delete - O(1)
+// insert - O(1)
+// linked lists are good for basic of other data structures, to represent graphs as an example
+
+public class LinkedList<T>
+{
+    private Node<T> _head;
+
+    public LinkedList(T value)
+    {
+        _head = new Node<T>(value);
+    }
+
+    public void AddFirst(T value)
+    {
+        var temp = _head;
+        var newNode = new Node<T>(value);
+
+        newNode.Next = temp;
+        temp.Prev = newNode;
+        _head = newNode;
+    }
+
+    public void AddLast(T value)
+    {
+        var temp = _head;
+        while (temp.Next != null)
+            temp = temp.Next;
+
+        temp.Next = new Node<T>(value);
+    }
+
+    public Node<T> FindFirst(T value)
+    {
+        var temp = _head;
+        while (temp.Next != null && !temp.Next.Value.Equals(value))
+            temp = temp.Next;
+
+        return temp;
+    }
+}
+
+// doubly linked list node
+public class Node<T>
+{
+    public T Value { get; set; }
+    public Node<T>? Next { get; set; }
+    public Node<T>? Prev { get; set; }
+
+    public Node(T value)
+    {
+        Value = value;
+    }
+}
