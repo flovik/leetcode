d1f24ccbeaf55d1f40a28ef44d9755938fcba304
Victor Florescu <victor.florescu@amdaris.com>
2024-06-15 16:41:43 +0300

    Maximum Gap - radix sort

---
+++ Solutions/Medium/MaximumGap.cs
@@ -0,0 +1,55 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class MaximumGapSol
+{
+    public int MaximumGap(int[] nums)
+    {
+        if (nums.Length < 2)
+            return 0;
+
+        var max = nums.Max();
+        int exp = 1; // 1, 10, 100, 1000, ...
+        var digits = 10;
+
+        var sortedArray = new int[nums.Length];
+
+        while (max / exp > 0)
+        {
+            var count = new int[digits];
+
+            // calculate each digit
+            foreach (var num in nums)
+            {
+                count[num / exp % 10]++;
+            }
+
+            // prefix sum
+            for (int i = 1; i < count.Length; i++)
+            {
+                count[i] += count[i - 1];
+            }
+
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                var cur = count[nums[i] / exp % 10] - 1;
+                sortedArray[cur] = nums[i];
+                count[nums[i] / exp % 10]--;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = sortedArray[i];
+            }
+
+            exp *= 10;
+        }
+
+        var maxResult = 0;
+        for (int i = 1; i < sortedArray.Length; i++)
+        {
+            maxResult = Math.Max(maxResult, sortedArray[i] - sortedArray[i - 1]);
+        }
+
+        return maxResult;
+    }
+}
---
+++ Topics/Sorting/RadixSort.cs
@@ -0,0 +1,31 @@
+﻿namespace Sandbox.Topics.Sorting;
+
+public class RadixSort
+{
+    public void Sort(int[] nums)
+    {
+        var max = nums.Max();
+        var count = new int[max + 1];
+
+        // count number of each number
+        foreach (var num in nums)
+        {
+            count[num]++;
+        }
+
+        // prefix sum
+        for (int i = 1; i < count.Length; i++)
+        {
+            count[i] = count[i - 1];
+        }
+
+        var sortedArray = new int[nums.Length];
+        for (var i = nums.Length - 1; i >= 0; i--)
+        {
+            var cur = nums[i];
+            var pos = count[cur] - 1;
+            sortedArray[pos] = cur;
+            count[cur]--;
+        }
+    }
+}
