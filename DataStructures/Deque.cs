4714fc270935699e8687aa15efaf4c209879c6ab
Victor Florescu <victor.florescu@amdaris.com>
2024-06-17 11:39:59 +0300

    circular deque

---
+++ DataStructures/Deque.cs
@@ -0,0 +1,79 @@
+﻿namespace Sandbox.DataStructures;
+
+public class MyCircularDeque
+{
+    private readonly LinkedList<int> _deque;
+    private int Capacity;
+
+    public MyCircularDeque(int k)
+    {
+        _deque = new LinkedList<int>();
+        Capacity = k;
+    }
+
+    public bool InsertFront(int value)
+    {
+        if (IsFull())
+            return false;
+
+        _deque.AddFirst(value);
+
+        return true;
+    }
+
+    public bool InsertLast(int value)
+    {
+        if (IsFull())
+            return false;
+
+        _deque.AddLast(value);
+
+        return true;
+    }
+
+    public bool DeleteFront()
+    {
+        if (IsEmpty())
+            return false;
+
+        _deque.RemoveFirst();
+
+        return true;
+    }
+
+    public bool DeleteLast()
+    {
+        if (IsEmpty())
+            return false;
+
+        _deque.RemoveLast();
+
+        return true;
+    }
+
+    public int GetFront()
+    {
+        if (IsEmpty())
+            return -1;
+
+        return _deque.First.Value;
+    }
+
+    public int GetRear()
+    {
+        if (IsEmpty())
+            return -1;
+
+        return _deque.Last.Value;
+    }
+
+    public bool IsEmpty()
+    {
+        return _deque.Count == 0;
+    }
+
+    public bool IsFull()
+    {
+        return _deque.Count == Capacity;
+    }
+}
