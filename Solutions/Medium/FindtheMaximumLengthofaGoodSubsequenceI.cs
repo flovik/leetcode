fa8885553ecacae200ee2aad7a855d439604c924
Victor Florescu <victor.florescu@amdaris.com>
2024-06-17 15:27:50 +0300

    fix

--- Solutions/Medium/FindtheMaximumLengthofaGoodSubsequenceI.cs
+++ Solutions/Medium/FindtheMaximumLengthofaGoodSubsequenceI.cs
@@ -5,11 +5,11 @@ public class FindtheMaximumLengthofaGoodSubsequenceI
     public int MaximumLength(int[] nums, int k)
     {
         // longest good subsequence with K different neighbors
-        var dp = new int[k + 1][];
+        var dp = new Dictionary<int, int>[k + 1];
 
         for (var i = 0; i <= k; i++)
         {
-            dp[i] = new int[nums.Length];
+            dp[i] = new Dictionary<int, int>(nums.Length);
         }
 
         // longest subsequence with K different neighbors
@@ -19,11 +19,15 @@ public class FindtheMaximumLengthofaGoodSubsequenceI
         {
             for (var j = k; j >= 0; j--)
             {
-                var v = dp[j][num];
+                var v = dp[j].GetValueOrDefault(num, 0);
                 var availableNeighbor = j > 0 ? result[j - 1] + 1 : 0;
 
                 v = Math.Max(v + 1, availableNeighbor);
-                dp[j][num] = v;
+                if (dp[j].ContainsKey(num))
+                    dp[j][num] = v;
+                else
+                    dp[j].Add(num, v);
+
                 result[j] = Math.Max(result[j], v);
             }
         }
