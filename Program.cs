a10b9a5553fe085c7095db894b16a7a6e9eda1b7
Victor Florescu <victor.florescu@amdaris.com>
2024-06-21 21:42:42 +0300

    Decode Ways

--- Program.cs
+++ Program.cs
@@ -2893,3 +2893,28 @@ using Sandbox.Topics.Trees;
 {
     // https://leetcode.com/problems/divisor-game/description/
 }
+
+{
+    // https://leetcode.com/problems/decode-ways/description/
+    var sol = NumDecodings("11106");
+    int NumDecodings(string s)
+    {
+        if (s[0] == '0')
+            return 0;
+
+        var dp = new int[s.Length + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= s.Length; i++)
+        {
+            if (s[i - 1] != '0')
+                dp[i] += dp[i - 1];
+
+            if (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] <= '6'))
+                dp[i] += dp[i - 2];
+        }
+
+        return dp[^1];
+    }
+}
