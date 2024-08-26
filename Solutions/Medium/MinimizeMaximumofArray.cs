cf4e53ef0e06fd9b94137b2e595bf54482c197e3
Victor Florescu <victor.florescu@amdaris.com>
2024-08-26 08:57:02 +0300

    Minimize Maximum of Array - elegant prefix sum

---
+++ Solutions/Medium/MinimizeMaximumofArray.cs
@@ -0,0 +1,19 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+internal class MinimizeMaximumofArray
+{
+    public int MinimizeArrayValue(int[] nums)
+    {
+        long curSum = nums[0];
+        long result = nums[0];
+
+        // prefix sum average, evenly distribute all the elements
+        for (int i = 1; i < nums.Length; i++)
+        {
+            curSum += nums[i];
+            result = Math.Max(result, (long) Math.Ceiling((double) curSum / (i + 1)));
+        }
+
+        return (int) result;
+    }
+}
