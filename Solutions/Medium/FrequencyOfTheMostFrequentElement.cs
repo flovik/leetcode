b4eda757ffa1829b0541eb5b00054c27a669b82e
Victor Florescu <victor.florescu@amdaris.com>
2024-10-11 21:58:39 +0300

    Frequency of the Most Frequent Element

---
+++ Solutions/Medium/FrequencyOfTheMostFrequentElement.cs
@@ -0,0 +1,31 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class FrequencyOfTheMostFrequentElement
+{
+    public int MaxFrequency(int[] nums, int k)
+    {
+        Array.Sort(nums);
+        int left = 0, right = 0, max = 0;
+        long sum = 0;
+
+        // sliding window + prefix sum
+        while (right < nums.Length)
+        {
+            var rangeFrequency = (long) nums[right] * (right - left);
+
+            if (rangeFrequency - sum > k)
+            {
+                sum -= nums[left];
+                left++;
+            }
+            else
+            {
+                sum += nums[right];
+                max = Math.Max(max, right - left + 1);
+                right++;
+            }
+        }
+
+        return max;
+    }
+}
