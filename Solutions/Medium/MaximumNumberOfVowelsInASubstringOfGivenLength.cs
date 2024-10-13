3518aa40942ec0a2e8d7ce9af86f0950175e86c3
Victor Florescu <victor.florescu@amdaris.com>
2024-10-13 18:15:46 +0300

    Maximum Number of Vowels in a Substring of Given Length

---
+++ Solutions/Medium/MaximumNumberOfVowelsInASubstringOfGivenLength.cs
@@ -0,0 +1,27 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class MaximumNumberOfVowelsInASubstringOfGivenLength
+{
+    public int MaxVowels(string s, int k)
+    {
+        int left = 0, right = 0, maxVowels = 0, vowels = 0;
+
+        while (right < s.Length)
+        {
+            if (IsVowel(s[right]))
+                vowels++;
+
+            if (right - left >= k && IsVowel(s[left++]))
+                vowels--;
+
+            if (right - left + 1 == k)
+                maxVowels = Math.Max(maxVowels, vowels);
+
+            right++;
+        }
+
+        return maxVowels;
+    }
+
+    private static bool IsVowel(char a) => a is 'a' or 'e' or 'i' or 'o' or 'u';
+}
