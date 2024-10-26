deec625aaee8d6edd85e91f034647c2bab7389ba
Victor Florescu <victor.florescu@amdaris.com>
2024-10-26 19:02:47 +0300

    Find the Original Typed String I

---
+++ Solutions/Easy/FindTheOriginalTypedStringI.cs
@@ -0,0 +1,21 @@
+﻿namespace Sandbox.Solutions.Easy;
+
+public class FindTheOriginalTypedStringI
+{
+    public int PossibleStringCount(string word)
+    {
+        // if a sub-sequence includes more characters, add its length - 1
+        var result = 1;
+        var ch = ' ';
+
+        foreach (var c in word)
+        {
+            if (ch == c)
+                result++;
+            else
+                ch = c;
+        }
+
+        return result;
+    }
+}
