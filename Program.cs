79075e3c048e3180e2be903c2a75c98c54144d62
Victor Florescu <victor.florescu@amdaris.com>
2024-12-01 09:15:14 +0200

    Combination Sum IV

--- Program.cs
+++ Program.cs
@@ -5214,3 +5214,27 @@ using Sandbox.Topics.Trees;
         return dp[n];
     }
 }
+
+{
+    // https://leetcode.com/problems/combination-sum-iv/
+    int CombinationSum4(int[] nums, int target)
+    {
+        var dp = new int[target + 1];
+
+        for (var i = 1; i <= target; i++)
+        {
+            foreach (var num in nums)
+            {
+                if (num > i)
+                    continue;
+
+                if (num == i)
+                    dp[i]++;
+
+                dp[i] += dp[i - num];
+            }
+        }
+
+        return dp[target];
+    }
+}
