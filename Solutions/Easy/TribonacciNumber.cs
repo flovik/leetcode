6d327dcb40f50e5855f7dc551430124b2879029a
Victor Florescu <victor.florescu@amdaris.com>
2024-12-07 11:10:26 +0200

    N-th Tribonacci Number

---
+++ Solutions/Easy/TribonacciNumber.cs
@@ -0,0 +1,22 @@
+﻿namespace Sandbox.Solutions.Easy;
+
+public class TribonacciNumber
+{
+    public int Tribonacci(int n)
+    {
+        if (n == 0)
+            return 0;
+
+        int a = 0, b = 1, c = 1, d = 1;
+
+        for (var i = 3; i <= n; i++)
+        {
+            d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
+        }
+
+        return d;
+    }
+}
