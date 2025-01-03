027c8c859d42ba8f9a1489afce230f299da6d537
Victor Florescu <victor.florescu@amdaris.com>
2025-01-03 16:34:26 +0200

    Number of Ways to Split Array

---
+++ Solutions/Medium/NumberOfWaysToSplitArray.cs
@@ -0,0 +1,25 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class NumberOfWaysToSplitArray
+{
+    public int WaysToSplitArray(int[] nums)
+    {
+        var prefix = new int[nums.Length];
+        Array.Copy(nums, prefix, nums.Length);
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            prefix[i] += prefix[i - 1];
+        }
+
+        var result = 0;
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            var diff = prefix[i] - prefix[^1];
+            if (prefix[i] > diff)
+                result++;
+        }
+
+        return result;
+    }
+}
