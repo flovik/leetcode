62b7fda7aaedf6b341b0fdd916245cdb52c1c9be
Victor Florescu <victor.florescu@amdaris.com>
2025-01-05 09:42:09 +0200

    Shifting Letters

---
+++ Solutions/Medium/ShiftingLetters.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace Sandbox.Solutions.Medium;
+
+internal class ShiftingLetters
+{
+    private char[] _letters = new char[26];
+
+    public string ShiftingLettersSol(string s, int[] shifts)
+    {
+        for (var i = 0; i < 26; i++)
+        {
+            _letters[i] = (char)('a' + i);
+        }
+
+        // prefix sum from right to left
+        for (int i = shifts.Length - 2; i >= 0; i--)
+        {
+            shifts[i] += shifts[i + 1] % 26;
+        }
+
+        var sb = new StringBuilder(s.Length);
+
+        for (var i = 0; i < shifts.Length; i++)
+        {
+            var index = s[i] - 'a';
+            var next = (index + shifts[i]) % 26;
+            sb.Append(_letters[next]);
+        }
+
+        return sb.ToString();
+    }
+}
