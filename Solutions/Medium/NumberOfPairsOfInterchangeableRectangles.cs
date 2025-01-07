b658126e914e04d927a668b95ef3c2018d7bd341
Victor Florescu <victor.florescu@amdaris.com>
2025-01-07 08:49:35 +0200

    Number of Pairs of Interchangeable Rectangles

---
+++ Solutions/Medium/NumberOfPairsOfInterchangeableRectangles.cs
@@ -0,0 +1,29 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class NumberOfPairsOfInterchangeableRectangles
+{
+    public long InterchangeableRectangles(int[][] rectangles)
+    {
+        // [width, height]
+        // interchangable - same width-to-height ratio
+        var dict = new Dictionary<double, List<int[]>>(rectangles.Length);
+        long pairs = 0;
+
+        foreach (var rectangle in rectangles)
+        {
+            if (rectangle[1] == 0)
+                continue;
+
+            var ratio = (double)rectangle[0] / rectangle[1];
+
+            dict.TryAdd(ratio, []);
+            var len = dict[ratio].Count;
+            pairs += len;
+
+            dict[ratio].Add(rectangle);
+        }
+
+        // number of pairs
+        return pairs;
+    }
+}
