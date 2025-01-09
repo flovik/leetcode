73ee542276c929f26ea14b4c703ae0a35ca47b0b
Victor Florescu <victor.florescu@amdaris.com>
2025-01-09 08:10:19 +0200

    Counting Words With a Given Prefix

---
+++ Solutions/Easy/CountingWordsWithAGivenPrefix.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.Solutions.Easy;
+
+public class CountingWordsWithAGivenPrefix
+{
+    public int PrefixCount(string[] words, string pref)
+    {
+        return words.Where(word => word.StartsWith(pref)).Count();
+    }
+}
