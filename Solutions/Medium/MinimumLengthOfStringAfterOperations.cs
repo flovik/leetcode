acf46b3e865a8650d2dce737cf6ec8ffdbcc6c21
Victor Florescu <victor.florescu@amdaris.com>
2025-01-13 08:09:52 +0200

    Minimum Length of String After Operations

---
+++ Solutions/Medium/MinimumLengthOfStringAfterOperations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.Solutions.Medium;
+
+internal class MinimumLengthOfStringAfterOperations
+{
+    public int MinimumLength(string s)
+    {
+        var dict = new int[26];
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            dict[s[i] - 'a']++;
+
+            if (dict[s[i] - 'a'] == 3)
+                dict[s[i] - 'a'] = 1;
+        }
+
+        return dict.Sum();
+    }
+}
