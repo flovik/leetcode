86f5a4eb6aeec9c2df0186c447b32b3a8a9ae6d0
Victor Florescu <victor.florescu@amdaris.com>
2025-01-19 13:48:55 +0200

    Repeated DNA Sequences

---
+++ Solutions/Medium/RepeatedDnaSequences.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.Solutions.Medium;
+
+public class RepeatedDnaSequences
+{
+    public IList<string> FindRepeatedDnaSequences(string s)
+    {
+        var dict = new Dictionary<string, int>(s.Length);
+        int left = 0, right = 9;
+
+        while (right < s.Length)
+        {
+            var str = s[left..(right + 1)];
+
+            dict.TryAdd(str, 0);
+            dict[str]++;
+            left++;
+            right++;
+        }
+
+        return dict.Where(e => e.Value > 1).Select(e => e.Key).ToList();
+    }
+}
