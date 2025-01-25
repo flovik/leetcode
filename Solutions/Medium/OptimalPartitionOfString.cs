51c6eff90444980c958ecd35936f84cf6ab6adc5
Victor Florescu <victor.florescu@amdaris.com>
2025-01-25 14:10:37 +0200

    Optimal Partition of String

---
+++ Solutions/Medium/OptimalPartitionOfString.cs
@@ -0,0 +1,25 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class OptimalPartitionOfString
+{
+    public int PartitionString(string s)
+    {
+        var chars = new int[26];
+        var result = 1;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            var ch = s[i] - 'a';
+
+            if (chars[ch] == 1)
+            {
+                result++;
+                Array.Clear(chars);
+            }
+
+            chars[ch]++;
+        }
+
+        return result;
+    }
+}
