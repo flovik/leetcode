949925a92e12994c5098c244c15a9b64f88623f7
Victor Florescu <victor.florescu@amdaris.com>
2025-01-28 08:55:56 +0200

    Minimum Penalty for a Shop

---
+++ Solutions/Medium/MinimumPenaltyForShop.cs
@@ -0,0 +1,46 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class MinimumPenaltyForShop
+{
+    public int BestClosingTime(string customers)
+    {
+        // YYNY, customer comes at hour 0 1 and 3
+        // for every hour shop is open and no customers, penalty++
+        // for every hour shop is closed and customers come, penalty++
+        // earliest hour to close for min penalty
+
+        // prefix sum + some counting
+        var n = customers.Length;
+        var prefixY = new int[n + 1];
+        var prefixN = new int[n + 1];
+
+        for (int i = customers.Length - 1; i >= 0; i--)
+        {
+            prefixY[i] += prefixY[i + 1];
+            prefixN[i] += prefixN[i + 1];
+
+            if (customers[i] == 'Y')
+                prefixY[i]++;
+            else
+                prefixN[i]++;
+        }
+
+        var result = int.MaxValue;
+        var index = 0;
+
+        for (int i = 0; i <= n; i++)
+        {
+            var y = prefixY[i];
+            var nPenalty = prefixN[0] - prefixN[i];
+            var penalty = y + nPenalty;
+
+            if (penalty < result)
+            {
+                result = penalty;
+                index = i;
+            }
+        }
+
+        return index;
+    }
+}
