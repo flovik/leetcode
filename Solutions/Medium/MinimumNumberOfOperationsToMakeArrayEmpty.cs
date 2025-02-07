155a9c7301b11c376872536ba81e8cd8c7097278
Victor Florescu <victor.florescu@amdaris.com>
2025-02-07 10:05:47 +0200

    Minimum Number of Operations to Make Array Empty

---
+++ Solutions/Medium/MinimumNumberOfOperationsToMakeArrayEmpty.cs
@@ -0,0 +1,29 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class MinimumNumberOfOperationsToMakeArrayEmpty
+{
+    public int MinOperations(int[] nums)
+    {
+        var result = 0;
+        var dict = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            dict.TryAdd(nums[i], 0);
+            dict[nums[i]]++;
+        }
+
+        foreach (var (_, count) in dict)
+        {
+            if (count == 1)
+                return -1;
+
+            if (count % 3 == 0)
+                result += count / 3;
+            else
+                result += count / 3 + 1;
+        }
+
+        return result;
+    }
+}
