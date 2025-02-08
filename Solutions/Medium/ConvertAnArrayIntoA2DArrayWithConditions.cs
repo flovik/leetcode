f58d6afa829b0c53c07731820ea8183652dabcfd
Victor Florescu <victor.florescu@amdaris.com>
2025-02-08 10:29:24 +0200

    Convert an Array Into a 2D Array With Conditions

---
+++ Solutions/Medium/ConvertAnArrayIntoA2DArrayWithConditions.cs
@@ -0,0 +1,33 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class ConvertAnArrayIntoA2DArrayWithConditions
+{
+    public IList<IList<int>> FindMatrix(int[] nums)
+    {
+        var result = new List<IList<int>>();
+        var dict = new int[201];
+
+        foreach (var num in nums)
+        {
+            dict[num]++;
+        }
+
+        while (dict.Any(e => e > 0))
+        {
+            var list = new List<int>();
+
+            for (var i = 0; i < 201; i++)
+            {
+                if (dict[i] == 0)
+                    continue;
+
+                list.Add(i);
+                dict[i]--;
+            }
+
+            result.Add(list);
+        }
+
+        return result;
+    }
+}
