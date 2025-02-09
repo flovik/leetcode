64da8f2e36d937664f419cbe838c14429ea42139
Victor Florescu <victor.florescu@amdaris.com>
2025-02-09 12:03:57 +0200

    Binary Subarrays With Sum - revise

---
+++ Solutions/Medium/BinarySubarraysWithSum.cs
@@ -0,0 +1,32 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class BinarySubarraysWithSum
+{
+    public int NumSubarraysWithSum(int[] nums, int goal)
+    {
+        // sliding window
+        // each combination of prefix zeroes contributes to the total count of subarrays
+        int count = 0, prefixZeroes = 0, left = 0, right = 0, curSum = 0;
+
+        while (right < nums.Length)
+        {
+            curSum += nums[right];
+            while (left < right && (nums[left] == 0 || curSum > goal))
+            {
+                if (nums[left] == 1)
+                    prefixZeroes = 0;
+                else
+                    prefixZeroes++;
+
+                curSum -= nums[left++];
+            }
+
+            if (curSum == goal)
+                count += 1 + prefixZeroes;
+
+            right++;
+        }
+
+        return count;
+    }
+}
