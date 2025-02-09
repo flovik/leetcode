d43e8eebff5caf6257648eb6c0c350818a4a6088
Victor Florescu <victor.florescu@amdaris.com>
2025-02-09 10:49:22 +0200

    Divide Array Into Arrays With Max Difference

---
+++ Solutions/Medium/DivideArrayIntoArraysWithMaxDifference.cs
@@ -0,0 +1,26 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class DivideArrayIntoArraysWithMaxDifference
+{
+    public int[][] DivideArray(int[] nums, int k)
+    {
+        Array.Sort(nums);
+
+        var list = new List<int[]>();
+
+        for (int i = 0; i < nums.Length; i += 3)
+        {
+            var arr = new int[3];
+            arr[0] = nums[i];
+            arr[1] = nums[i + 1];
+            arr[2] = nums[i + 2];
+
+            if (arr[2] - arr[1] > k || arr[2] - arr[0] > k || arr[1] - arr[0] > k)
+                return [];
+
+            list.Add(arr);
+        }
+
+        return [.. list];
+    }
+}
