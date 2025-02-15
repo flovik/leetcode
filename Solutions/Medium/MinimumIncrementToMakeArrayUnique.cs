6eb4e4eaf68041ea40011c2132b8f46bf65f8b5e
Victor Florescu <victor.florescu@amdaris.com>
2025-02-15 12:15:53 +0200

    Minimum Increment to Make Array Unique

---
+++ Solutions/Medium/MinimumIncrementToMakeArrayUnique.cs
@@ -0,0 +1,25 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class MinimumIncrementToMakeArrayUnique
+{
+    public int MinIncrementForUnique(int[] nums)
+    {
+        var result = 0;
+
+        Array.Sort(nums);
+        var maxSoFar = -1;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] <= maxSoFar)
+            {
+                result += (maxSoFar + 1) - nums[i];
+                nums[i] = maxSoFar + 1;
+            }
+
+            maxSoFar = nums[i];
+        }
+
+        return result;
+    }
+}
