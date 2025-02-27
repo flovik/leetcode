9e98c21f709c5b5780af5154247e8b0875935adf
Victor Florescu <victor.florescu@amdaris.com>
2025-02-27 08:39:01 +0200

    Count Subarrays Where Max Element Appears at Least K Times

---
+++ Solutions/Medium/CountSubarraysWhereMaxElementAppearsAtLeastKTimes.cs
@@ -0,0 +1,31 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class CountSubarraysWhereMaxElementAppearsAtLeastKTimes
+{
+    public long CountSubarrays(int[] nums, int k)
+    {
+        long result = 0;
+        int max = nums.Max(), left = 0, right = 0, curK = 0;
+
+        while (right < nums.Length)
+        {
+            if (nums[right] == max)
+                curK++;
+
+            while (curK == k)
+            {
+                var value = nums.Length - right;
+                result += value;
+
+                if (nums[left] == max)
+                    curK--;
+
+                left++;
+            }
+
+            right++;
+        }
+
+        return result;
+    }
+}
