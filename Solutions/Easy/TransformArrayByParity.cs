3689b80a9c3dbe2e92af17e4319589bb676a562e
Victor Florescu <victor.florescu@amdaris.com>
2025-03-02 15:38:12 +0200

    Transform array by parity

--- Solutions/Easy/TransformArrayByParity.cs
+++ Solutions/Easy/TransformArrayByParity.cs
@@ -5,6 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Sandbox.Solutions.Easy;
+
 internal class TransformArrayByParity
 {
+    public int[] TransformArray(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] % 2 == 0)
+                nums[i] = 0;
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] % 2 == 1)
+                nums[i] = 1;
+        }
+
+        Array.Sort(nums);
+        return nums;
+    }
 }
