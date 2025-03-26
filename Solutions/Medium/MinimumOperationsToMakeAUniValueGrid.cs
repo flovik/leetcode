7c22802deac03e2d6456b21b4a262d4b16f54822
Victor Florescu <victor.florescu@isa.utm.md>
2025-03-26 11:19:24 +0200

    Minimum Operations to Make a Uni-Value Grid

---
+++ Solutions/Medium/MinimumOperationsToMakeAUniValueGrid.cs
@@ -0,0 +1,29 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class MinimumOperationsToMakeAUniValueGrid
+{
+    public int MinOperations(int[][] grid, int x)
+    {
+        // get sum of grid and get the average
+        int result = 0;
+
+        var arr = grid.SelectMany(e => e).ToArray();
+        Array.Sort(arr);
+
+        var middle = arr[(arr.Length - 1) / 2];
+
+        for (var i = 0; i < arr.Length; i++)
+        {
+            var diff = Math.Abs(arr[i] - middle);
+
+            // that number cannot be made into the average
+            if (diff % x != 0)
+                return -1;
+
+            var operations = diff / x;
+            result += operations;
+        }
+
+        return result;
+    }
+}
