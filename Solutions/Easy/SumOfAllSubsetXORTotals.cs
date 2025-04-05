01a97de0d242b836181f7bba5722d51bae8c1162
Victor Florescu <victor.florescu@amdaris.com>
2025-04-05 14:58:26 +0300

    Sum of All Subset XOR Totals

---
+++ Solutions/Easy/SumOfAllSubsetXORTotals.cs
@@ -0,0 +1,28 @@
+﻿namespace Sandbox.Solutions.Easy;
+
+public class SumOfAllSubsetXORTotals
+{
+    public int SubsetXORSum(int[] nums)
+    {
+        // sum of all XOR totals for every subset
+        // ^ = xor
+
+        // pass the index and then decide to take it or skip it
+        // if you take it, apply xor on it (start with 1)
+        return Backtrack(0, 0);
+
+        int Backtrack(int index, int num)
+        {
+            // base case, out-of-bounds
+            if (index == nums.Length)
+                return 0;
+
+            var take = nums[index] ^ num;
+
+            var next = Backtrack(index + 1, take);
+            var skip = Backtrack(index + 1, num);
+
+            return take + next + skip;
+        }
+    }
+}
