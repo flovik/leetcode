59f5d373fb5cfbcbd1535dcef9b0e3edb51d3fc3
Victor Florescu <victor.florescu@amdaris.com>
2025-04-28 09:51:17 +0300

    Maximum Beauty of an Array After Applying Operation

---
+++ Solutions/Medium/MaximumBeautyOfAnArrayAfterApplyingOperation.cs
@@ -0,0 +1,21 @@
+﻿namespace Sandbox.Solutions.Medium;
+public class MaximumBeautyOfAnArrayAfterApplyingOperation
+{
+    public int MaximumBeauty(int[] nums, int k)
+    {
+        Array.Sort(nums);
+        var len = 0;
+        int left = 0, right = 0;
+
+        while (right < nums.Length)
+        {
+            while (nums[right] - nums[left] > 2 * k)
+                left++;
+
+            len = Math.Max(len, right - left + 1);
+            right++;
+        }
+
+        return len;
+    }
+}
