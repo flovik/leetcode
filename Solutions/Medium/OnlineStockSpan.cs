d403e0e4e49855cb8ec1ce98616b5fa491190003
Victor Florescu <victor.florescu@amdaris.com>
2025-05-12 08:33:00 +0300

    Online Stock Span

---
+++ Solutions/Medium/OnlineStockSpan.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Sandbox.Solutions.Medium;
+
+public class StockSpanner
+{
+    private int _index;
+    private readonly Stack<(int, int)> _st;
+
+    public StockSpanner()
+    {
+        _st = new Stack<(int, int)>(10000);
+    }
+
+    public int Next(int price)
+    {
+        int value;
+        while (_st.Count > 0 && _st.TryPeek(out var tuple) && tuple.Item1 <= price)
+            _st.Pop();
+
+        if (_st.Count > 0)
+            value = _index - _st.Peek().Item2;
+
+        // bigger than the whole prev values
+        else
+            value = _index + 1;
+
+        _st.Push((price, _index));
+        _index++;
+
+        return value;
+    }
+}
