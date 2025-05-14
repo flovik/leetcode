3797cadc285b3b32781676bef148b0189a4e0f66
Victor Florescu <victor.florescu@amdaris.com>
2025-05-14 08:21:56 +0300

    Seat Reservation Manager

---
+++ Solutions/Medium/SeatReservationManager.cs
@@ -0,0 +1,27 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class SeatManager
+{
+    private readonly PriorityQueue<int, int> _availableSeats;
+
+    public SeatManager(int n)
+    {
+        _availableSeats = new PriorityQueue<int, int>(n);
+
+        for (var i = 1; i <= n; i++)
+        {
+            _availableSeats.Enqueue(i, i);
+        }
+    }
+
+    public int Reserve()
+    {
+        var smallest = _availableSeats.Dequeue();
+        return smallest;
+    }
+
+    public void Unreserve(int seatNumber)
+    {
+        _availableSeats.Enqueue(seatNumber, seatNumber);
+    }
+}
