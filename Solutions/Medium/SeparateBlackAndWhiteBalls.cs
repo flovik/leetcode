e6414f6f3bbe5fee5e077c2b4c47b64e70f89801
Victor Florescu <victor.florescu@amdaris.com>
2025-06-04 08:52:26 +0300

    Separate Black and White Balls

---
+++ Solutions/Medium/SeparateBlackAndWhiteBalls.cs
@@ -0,0 +1,26 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class SeparateBlackAndWhiteBalls
+{
+    public long MinimumSteps(string s)
+    {
+        long count = 0;
+
+        int left = 0, right = s.Length - 1;
+
+        while (left < right)
+        {
+            while (left < right && s[right] == '1')
+                right--;
+
+            while (left < right & s[left] == '0')
+                left++;
+
+            count += right - left;
+            left++;
+            right--;
+        }
+
+        return count;
+    }
+}
