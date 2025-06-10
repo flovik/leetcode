33d2102d991e065aa3f12f046b3f8e1d6a279826
Victor Florescu <victor.florescu@amdaris.com>
2025-06-10 08:48:40 +0300

    Arithmetic Slices

--- Program.cs
+++ Program.cs
@@ -7599,3 +7599,15 @@ using TaskScheduler = Sandbox.Solutions.Medium.TaskScheduler;
     var sol = new MinimumDifficultyOfAJobSchedule();
     sol.MinDifficulty([6, 5, 4, 3, 2, 1], 2);
 }
+
+{
+    // https://leetcode.com/problems/arithmetic-slices/
+    var sol = new ArithmeticSlices();
+    sol.NumberOfArithmeticSlices([1, 1, 0, 2, 1, 1, 0, 0, 0, 2, 2, 1, 2, 1, 2, 3]);
+}
+
+{
+    // https://leetcode.com/problems/arithmetic-slices-ii-subsequence/description/
+    var sol = new ArithmeticSlices2();
+    sol.NumberOfArithmeticSlices([2, 4, 6, 8, 10]);
+}
---
+++ Solutions/Medium/ArithmeticSlices.cs
@@ -0,0 +1,36 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class ArithmeticSlices
+{
+    public int NumberOfArithmeticSlices(int[] nums)
+    {
+        if (nums.Length < 3)
+            return 0;
+
+        // each number has their own sequences, when you compute difference with current number, take only the sequences
+        // equal to the current difference
+        var dp = new Dictionary<int, int>[nums.Length];
+        var count = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            dp[i] = [];
+        }
+
+        dp[1].Add(nums[1] - nums[0], 1);
+
+        for (var i = 2; i < nums.Length; i++)
+        {
+            var seq = nums[i] - nums[i - 1];
+
+            if (dp[i - 1].TryGetValue(seq, out var len))
+            {
+                count += len;
+                dp[i].TryAdd(seq, len + 1);
+            }
+            else dp[i].TryAdd(seq, 1);
+        }
+
+        return count;
+    }
+}
