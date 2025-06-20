747662bd7409dc3c654ace63ccef6b73f0b774c1
Victor Florescu <victor.florescu@amdaris.com>
2025-06-20 08:54:21 +0300

    Minimum Difference Between Largest and Smallest Value in Three Moves

---
+++ Solutions/Medium/MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.cs
@@ -0,0 +1,28 @@
+﻿namespace Sandbox.Solutions.Medium;
+
+public class MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves
+{
+    public int MinDifference(int[] nums)
+    {
+        if (nums.Length <= 4) return 0;
+
+        // min difference between min and max in nums, at most 3 moves
+        Array.Sort(nums);
+
+        // get min and max in the remaining window of sorted numbers, when 3 are removed
+        var windowLength = nums.Length - 3;
+
+        int left = 0, right = windowLength - 1;
+        var min = int.MaxValue;
+
+        while (right < nums.Length)
+        {
+            min = Math.Min(min, nums[right] - nums[left]);
+
+            left++;
+            right++;
+        }
+
+        return min;
+    }
+}
